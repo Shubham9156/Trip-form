5d84846e4b846c3f7beb5d543f73d152dcfb535b
Shubham Gade <87603025+Shubham9156@users.noreply.github.com>
2021-11-19 11:05:07 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+$insert = false;
+// Set connection variables
+if(isset($_POST['name'])){
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con)
+    {
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    //echo "Success connecting to the db";
+
+    // collect post variables
+    $name = $_POST['name'];
+    $gender = $_POST['gender'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $desc = $_POST['desc'];
+    $sql = "INSERT INTO `trip`.`trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dd`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$desc', current_timestamp());";
+    //echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true)
+    {
+        //echo "Successfully Inserted";
+
+        // Flag for insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+    // Close the database connection
+    $con->close();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To Travel Form</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Praise&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <img class="bg" src="bg.png" alt="college">
+    <div class="container">
+        <h1>Welcome To Trip form</h1>
+        <p>Enter your details and submit this form to confirm your participation in the trip </p>
+        <?php
+        if($insert == true){
+        echo "<p class='submitMsg'>Thanks for ssubmitting your form. We are happy to see you joing for the US trip</p>";
+        }
+    ?>
+        <form action="index.php" method="post">
+            <input type="text" name="name" id="name" placeholder="Enter your name ">
+            <input type="text" name="age" id="age" placeholder="Enter your age ">
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender ">
+            <input type="text" name="email" id="email" placeholder="Enter your email ">
+            <input type="text" name="phone" id="phone" placeholder="Enter your phone ">
+            <textarea name="desc" id="" cols="30" rows="10" placeholder="Enter any other information here"></textarea>
+            <button class="btn">Submit</button>
+            
+        </form>
+    </div>
+    <script src="index.js"></script>
+    
+</body>
+</html>
